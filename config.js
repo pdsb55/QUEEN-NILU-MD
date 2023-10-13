1e6176a009673b8dfc33505f679107183c4a166f
pdsb55 <147835049+pdsb55@users.noreply.github.com>
2023-10-13 19:05:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = '' // PUT your session ID BY scan QR 
+global.SESSION_ID = 'QueenNilu;;;cqpAGTZZ#82rWCwB5nw35CR8JWy13dtdFI_sTTc2yG_vBnognAWM' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
 global.POSTGRESQL_URL = ''
